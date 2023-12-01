e0e7736d57ea61cefbd633276167c5bf6f34f7e8
ninuinu <helena.rosenzw@gmail.com>
2023-12-01 23:27:53 +0100

    Add solution for first puzzle day one

---
+++ src/one/solution.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"unicode"
+)
+
+func main() {
+	file, _ := os.Open("input.txt")
+	fscanner := bufio.NewScanner(file)
+	
+	var totalSum int = 0
+	for fscanner.Scan(){
+		line := fscanner.Text()
+
+		var left string
+		var right string
+
+		
+		for _, character := range line{
+			if unicode.IsDigit(character){
+				left = string(character)
+				break
+			}
+			fmt.Print(left)
+		}
+
+		runes := []rune(line) 
+		for i := len(runes) - 1; i >= 0; i-- {
+			if unicode.IsDigit(runes[i]){
+				right = string(runes[i])
+				break
+			}
+			fmt.Print(right)
+		}
+
+		num := left + right
+		if n, err := strconv.Atoi(num); err == nil {
+			totalSum += n
+		}
+	}
+	fmt.Printf("%d", totalSum)
+}
+
